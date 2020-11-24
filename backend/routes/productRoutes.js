6a3c08ac4d497dcc0b028f2e86cbbc059873c681
dnirns <daniel.irons@gmail.com>
2020-11-24 18:00:20 +0000

    backend routes finished

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,33 @@
+import express from 'express'
+import asyncHandler from 'express-async-handler'
+const router = express.Router()
+import Product from '../models/productModel.js'
+
+
+
+//*ALL PRODUCTS ROUTE
+router.get(
+  '/',
+  asyncHandler(async (req, res) => {
+    const products = await Product.find({})
+
+    res.json(products)
+  })
+)
+
+//*PRODUCT ROUTE
+router.get(
+  '/:id',
+  asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id)
+
+    if (product) {
+      res.json(product)
+    } else {
+      res.status(404)
+      throw new Error('Product not Found')
+    }
+  })
+)
+
+export default router
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import products from'./data/products.js'
 import connectDB from './config/db.js'
+import productRoutes from './routes/productRoutes.js'
+import { errorHandler, notFound } from './middleware/errorHandlers.js'
 
 //? LOAD DOTENV VARIABLES
 dotenv.config()
@@ -17,19 +18,15 @@ app.get('/', (req, res) => {
   res.send('api running')
 })
 
-//* GET ALL PRODUCTS ROUTE
-app.get('/api/products', (req, res) => {
-  res.json(products)
-})
+app.use('/api/products', productRoutes)
 
-//* GET SINGLE PRODUCT ROUTE
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((product) => product._id === req.params.id)
-  res.json(product)
-})
+//?ERROR HANDLER FUNCTIONS
+app.use(notFound)
+
+app.use(errorHandler)
 
 //? START SERVER
 
 //dotenv variable for port number
-const PORT =  process.env.PORT || 5000
+const PORT = process.env.PORT || 5000
 app.listen(PORT, console.log(`server running on port ${PORT}`))
