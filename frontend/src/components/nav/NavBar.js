ac91c40fb8a4401aa75f50c27891787ffa9b1c39
dnirns <daniel.irons@gmail.com>
2020-11-30 16:38:48 +0000

    hide nav on scroll

--- frontend/src/components/nav/NavBar.js
+++ frontend/src/components/nav/NavBar.js
@@ -1,13 +1,26 @@
 import { useDispatch, useSelector } from 'react-redux'
 import { openDrawer } from '../../actions/navActions'
 import { openCart } from '../../actions/cartActions'
-import { Toolbar, AppBar, Box, IconButton } from '@material-ui/core'
+import { Toolbar, AppBar, Box, IconButton, Slide } from '@material-ui/core'
 import MenuIcon from '@material-ui/icons/Menu'
 import LocalMallIcon from '@material-ui/icons/LocalMall'
 import Button from '@material-ui/core/Button'
 import Badge from '@material-ui/core/Badge'
 import { makeStyles } from '@material-ui/core/styles'
 import { Link } from 'react-router-dom'
+
+//hide on scroll
+import useScrollTrigger from '@material-ui/core/useScrollTrigger'
+
+const HideOnScroll = ({ children, window }) => {
+  const trigger = useScrollTrigger({ target: window ? window() : undefined })
+
+  return (
+    <Slide appear={false} direction='down' in={!trigger}>
+      {children}
+    </Slide>
+  )
+}
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -39,39 +52,41 @@ const NavBar = () => {
   const classes = useStyles()
 
   return (
-    <AppBar
-      position='sticky'
-      className={classes.root}
-      style={{ backgroundColor: '#1b1b1b' }}
-    >
-      <Toolbar variant='dense'>
-        <Box display={{ sm: 'block', md: 'none' }}>
-          <div aria-haspopup='true' onClick={handleOpenDrawer}>
-            <IconButton edge='start' color='inherit' aria-label='menu'>
-              <MenuIcon fontSize='large' />
-            </IconButton>
-          </div>
-        </Box>
-        <Box display={{ xs: 'none', sm: 'none', md: 'block' }}>
-          <Link className={classes.menuLink} to='/'>
-            <Button className={classes.menuLink}>HOME</Button>
-          </Link>
-          <Link className={classes.menuLink} to='/products'>
-            <Button className={classes.menuLink}>PRODUCTS</Button>
-          </Link>
-        </Box>
+    <HideOnScroll>
+      <AppBar
+        position='sticky'
+        className={classes.root}
+        style={{ backgroundColor: '#1b1b1b' }}
+      >
+        <Toolbar variant='dense'>
+          <Box display={{ sm: 'block', md: 'none' }}>
+            <div aria-haspopup='true' onClick={handleOpenDrawer}>
+              <IconButton edge='start' color='inherit' aria-label='menu'>
+                <MenuIcon fontSize='large' />
+              </IconButton>
+            </div>
+          </Box>
+          <Box display={{ xs: 'none', sm: 'none', md: 'block' }}>
+            <Link className={classes.menuLink} to='/'>
+              <Button className={classes.menuLink}>HOME</Button>
+            </Link>
+            <Link className={classes.menuLink} to='/products'>
+              <Button className={classes.menuLink}>PRODUCTS</Button>
+            </Link>
+          </Box>
 
-        <Box className={classes.icon}>
-          <Link>
-            <Button className={classes.menuLink} onClick={handleOpenCart}>
-              <Badge badgeContent={totalItems} color='primary'>
-                <LocalMallIcon />
-              </Badge>
-            </Button>
-          </Link>
-        </Box>
-      </Toolbar>
-    </AppBar>
+          <Box className={classes.icon}>
+            <Link>
+              <Button className={classes.menuLink} onClick={handleOpenCart}>
+                <Badge badgeContent={totalItems} color='primary'>
+                  <LocalMallIcon />
+                </Badge>
+              </Button>
+            </Link>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </HideOnScroll>
   )
 }
 export default NavBar
